c20b94b749dfdedec5c92aed39d6bbce82d1712e
unknown <irfantrianda522@gmail.com>
2023-10-01 13:18:13 +0700

    fisrt commit

---
+++ src/com/example/Tugas_Irfan_Trianda.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Tugas_Irfan_Trianda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Menampilkan nama dan NIM
+        String nama = "Irfan Trianda";
+        String nim = "2311533009";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+	}
+
+}
